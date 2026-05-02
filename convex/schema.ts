d2f529615c2576b8ff029e290ce1b70c54f5cb69
maffiakhateguaro <maffiakhateg@gmail.com>
2026-05-02 09:20:15 +0800

    Done configuring the environment variables

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,13 +2,13 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  users: defineTable({
+    // add fields later if needed
+  }),
+
   todos: defineTable({
     text: v.string(),
     isCompleted: v.boolean(),
-    userId: v.id("users")
+    userId: v.optional(v.id("users")),
   }),
-  users: defineTable({
-    username: v.string(),
-    password: v.string(),
-  })
 });
